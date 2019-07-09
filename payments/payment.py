6db8b3ddf145ae755cb575fd3941f90c51e47895
Makeev Ivan <makeev@phystech.edu>
2019-07-09 21:56:23 +0300

    payments working with test_provider

--- payments/payment.py
+++ payments/payment.py
@@ -1,40 +1,34 @@
 from bot import bot
-from telebot.types import LabeledPrice, ShippingOption
+from telebot.types import LabeledPrice
 import settings
 
-
 provider_token = settings.PROVIDER_TOKEN
-prices = [LabeledPrice(label='Working Time Machine', amount=100)]
-shipping_options = [
-    ShippingOption(id='instant', title='WorldWide Teleporter').add_price(LabeledPrice('Teleporter', 1000)),
-    ShippingOption(id='pickup', title='Local pickup').add_price(LabeledPrice('Pickup', 0))]
 
 
-@bot.message_handler(commands=['pay'])
-def pay(message):
-    bot.send_invoice(message.chat.id,
-                     title='Working Time Machine',
+prices = [LabeledPrice(label='Working Time Machine', amount=200)]
+
+
+@bot.message_handler(commands=['buy'])
+def command_pay(message):
+    bot.send_message(message.chat.id,
+                     "Real cards won't work with me, no money will be debited from your account."
+                     " Use this test card number to pay for your Time Machine: `4242 4242 4242 4242`"
+                     "\n\nThis is your demo invoice:", parse_mode='Markdown')
+    bot.send_invoice(message.chat.id, title='Working Time Machine',
                      description='Want to visit your great-great-great-grandparents?'
-                                     ' Make a fortune at the races?'
-                                     ' Shake hands with Hammurabi and take a stroll in the Hanging Gardens?'
-                                     ' Order our Working Time Machine today!',
+                                 ' Make a fortune at the races?'
+                                 ' Shake hands with Hammurabi and take a stroll in the Hanging Gardens?'
+                                 ' Order our Working Time Machine today!',
                      provider_token=provider_token,
-                         currency='usd',
-                         photo_url='http://erkelzaar.tsudao.com/models/perrotta/TIME_MACHINE.jpg',
-                         photo_height=512,  # !=0/None or picture won't be shown
-                         photo_width=512,
-                         photo_size=512,
-                         is_flexible=False,  # True If you need to set up Shipping Fee
-                         prices=prices,
-                         start_parameter='time-machine-example',
-                         invoice_payload='HAPPY FRIDAYS COUPON')
-
-
-@bot.shipping_query_handler(func=lambda query: True)
-def shipping(shipping_query):
-    print(shipping_query)
-    bot.answer_shipping_query(shipping_query.id, ok=True, shipping_options=shipping_options,
-                              error_message='Oh, seems like our Dog couriers are having a lunch right now. Try again later!')
+                     currency='usd',
+                     photo_url='http://erkelzaar.tsudao.com/models/perrotta/TIME_MACHINE.jpg',
+                     photo_height=512,  # !=0/None or picture won't be shown
+                     photo_width=512,
+                     photo_size=512,
+                     is_flexible=False,  # True If you need to set up Shipping Fee
+                     prices=prices,
+                     start_parameter='time-machine-example',
+                     invoice_payload='HAPPY FRIDAYS COUPON')
 
 
 @bot.pre_checkout_query_handler(func=lambda query: True)
