ad0e930ccc344318cfa2393265f74b204b7aad35
Makeev Ivan <makeev@phystech.edu>
2019-07-10 16:05:10 +0300

    add logging to bot

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import telebot
 import settings
+import logging
 
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
 
 if settings.DEBUG:
     telebot.apihelper.proxy = {'https': settings.PROXY}
